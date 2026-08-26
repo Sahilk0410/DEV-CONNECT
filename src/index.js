6de419ce8a3ccadfbc2c4ef479d94a4d8cf31868
Sahil <cenaloversahil02@gmail.com>
2026-08-26 21:53:28 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express()
+
+app.get("/user", (req, res) => {
+    res.send("user is logged in")
+    console.log("user is loggen in")
+})
+
+app.listen(3000, () => {
+    console.log(`server is running on port 3000`)
+})
