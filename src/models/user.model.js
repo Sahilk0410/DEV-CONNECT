428f643a35d72f7e4d9cf7e38510c39d4bdf3142
Sahil <cenaloversahil02@gmail.com>
2026-08-27 18:05:37 +0530

    tokens and password encryption

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -39,4 +39,51 @@ const userSchema = new Schema({
 
 }, {timestamps: true}) 
 
+
+// this method will encypts the password before saving into the database
+userSchema.pre("Save", async function (next) {
+
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+
+// comparing sended password and saved password
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+// generates access token
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+
+// generates refresh token
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model("User", userSchema)
