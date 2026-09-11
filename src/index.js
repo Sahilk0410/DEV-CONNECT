7499e60623875578d02c241c912efbe815312d57
Sahil <cenaloversahil02@gmail.com>
2026-09-11 09:51:09 +0530

    user registration

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ connectDB()
 .then( () => {
     app.listen(process.env.PORT || 8000, () => {
         console.log(`server is running on port: ${process.env.PORT}`)
+        
+    })
+
+    app.get("/users", (req, res) => {
+        res.send("hello")
     })
 } )
 .catch( (error) => {
